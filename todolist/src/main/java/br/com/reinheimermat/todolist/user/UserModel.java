50c2f36c643b15625b68f4f24972c93b57da42a4
reinheimermat <mreinheimer0@gmail.com>
2023-10-10 18:50:04 -0300

    lobok-lib-add

--- todolist/src/main/java/br/com/reinheimermat/todolist/user/UserModel.java
+++ todolist/src/main/java/br/com/reinheimermat/todolist/user/UserModel.java
@@ -1,31 +1,9 @@
 package br.com.reinheimermat.todolist.user;
 
+// import lombok.Data;
+// @Data
 public class UserModel {
     public String username;
     public String name;
     public String password;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
